e461015bb0de7a857f5331539356002bfee5ecb6
YanisMD <yanismohamed@MacBook-Pro-de-Yanis.local>
2025-10-06 15:41:45 +0200

    [TU & AOP] Création de Logging pour Service.impl.method, TU de DomainRepository

--- src/main/java/com/example/revision_app/aspect/LoggingAspect.java
+++ src/main/java/com/example/revision_app/aspect/LoggingAspect.java
@@ -7,9 +7,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Aspect
 @Component
